868df9b5706bdc0580781eef8005687d724c0ff6
Chiara <chiara.emina@studio.unibo.it>
2025-11-18 12:17:18 +0100

    create setCurrentFile() and getCurrentFile()

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,18 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private File currentFile;
 
+    public void setCurrentFile(final File file){
+        this.currentFile = file;
+    }
+
+    public File getCurrentFile(){
+        return this.currentFile;
+    }
 }
