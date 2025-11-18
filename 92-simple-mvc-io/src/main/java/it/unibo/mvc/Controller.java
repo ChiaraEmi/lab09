615001efa7c97b47455d7bf1242c815bd54cfe08
Chiara <chiara.emina@studio.unibo.it>
2025-11-18 13:59:45 +0100

    create saveString() and add constructor

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,12 +1,24 @@
 package it.unibo.mvc;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.swing.JOptionPane;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String PATH = System.getProperty("user.home")
+            + File.separator
+            + "output.txt";
     private File currentFile;
+
+    Controller() {
+        this.currentFile = new File(PATH);
+    }
 
     public void setCurrentFile(final File file) {
         this.currentFile = file;
@@ -18,5 +30,11 @@ public class Controller {
 
     public String getFilePath (final File file) {
         return this.currentFile.getPath();
+    }
+
+    public void saveString (final String text) throws IOException {
+        try (PrintStream ps = new PrintStream(currentFile, StandardCharsets.UTF_8)) {
+            ps.print(text);
+        }
     }
 }
