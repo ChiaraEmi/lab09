fec591691d667b308e29e310b0145f3249a7f818
Chiara <chiara.emina@studio.unibo.it>
2025-11-19 17:21:30 +0100

    added the methods setNextString() and getNextString() in SimpleController

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -6,4 +6,23 @@ package it.unibo.mvc;
  */
 public final class SimpleController implements Controller {
 
+    private String nextString = new String();
+
+    public void setNextString(final String s) {
+        if (s == null) {
+            throw new NullPointerException();
+        }
+
+        this.nextString = s;
+    }
+
+    public String getNextString() {
+        return this.nextString;
+    }
+    
+    @Override
+    public void print(String text) {
+        System.out.println(text);
+    }
+
 }
